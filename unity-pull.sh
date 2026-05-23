b6cb4a93f52764f0226a46f1f346fe083c49fcb8
Randy Westmoreland <randy.westmoreland@gmail.com>
2026-05-23 11:15:02 -0500

    Script push/pull update.

--- unity-pull.sh
+++ unity-pull.sh
@@ -4,5 +4,5 @@ rsync -av --delete \
 --exclude Logs \
 --exclude Temp \
 --exclude obj \
-randy@192.168.68.68:~/Documents/Workspace/Unity-Projects/Tekken-8-Fighting-Game-Clone/ \
-~/Documents/Workspace/Unity-Projects/Tekken-8-Fighting-Game-Clone/
+randy@10.10.10.2:~/Documents/Workspace/Unity-Projects/Knockout/ \
+~/Documents/Workspace/Unity-Projects/Knockout/
